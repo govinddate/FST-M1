c193c331e06b670ae3570dfd9b22f760eda19fe7
govind_date07 <61514332+govinddate@users.noreply.github.com>
2021-11-27 01:51:55 +0530

    Update Activity6.java

--- Selenium/Project/Activity6.java
+++ Selenium/Project/Activity6.java
@@ -1,40 +1,47 @@
-package Project_Selenium;
+package Demo.Project_HRM;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Activity6 {
+public class Activity_6 {
+	WebDriver driver;
+	  @Test
+	  public void f() throws InterruptedException {
+		  driver.findElement(By.id("txtUsername")).sendKeys("orange");
+		  driver.findElement(By.id("txtPassword")).sendKeys("orangepassword123");
+		  driver.findElement(By.id("btnLogin")).click();
+		  Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id=\"menu_directory_viewDirectory\"]/b")).isDisplayed();
+		WebDriverWait wait = new WebDriverWait(driver, 10); 
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"menu_directory_viewDirectory\"]/b")));
+		element.click();
+		 // WebElement s=driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div[1]/h1"));
+		String s=driver.findElement(By.xpath(("//*[@id=\"content\"]/div[1]/div[1]/h1"))).getText();
+		 Assert.assertEquals("Search Directory",s);
+		 System.out.println("Activity 6 executed successfully");
+	  }
+	  @BeforeMethod
+	  public void beforeMethod() {
+		  driver=new FirefoxDriver();
+		  driver.navigate().to("http://alchemy.hguy.co:8080/orangehrm/symfony/web/index.php/auth/login");
+		
+		 
+		  
+	  }
 
-	@Test
-	public static void main() 
-	{
-		
-		WebDriver driver = new FirefoxDriver();
-		driver.get("https://alchemy.hguy.co/lms");
-		
-		driver.findElement(By.linkText("My Account")).click();
-		
-        String Title = driver.getTitle();
-		boolean status = Title.contentEquals("My Account – Alchemy LMS");
-		System.out.println("Title Matches: " + status);
-		
-		driver.findElement(By.xpath("//*[@id=\"uagb-column-e9d225cb-cee9-4e02-a12d-073d5f051e91\"]/div[2]/div[2]/a")).click();
-		
-		driver.findElement(By.id("user_login")).sendKeys("root");
-		driver.findElement(By.id("user_pass")).sendKeys("pa$$w0rd");
-		
-		driver.findElement(By.className("login-submit")).click();
-		
-		 String element = driver.findElement(By.xpath("//*[@id=\"wp-admin-bar-my-account\"]/a")).getText();
-		
-	     boolean matches = element.contentEquals("Howdy, root");
-			
-			System.out.println("User Matches: " + matches);
-			
-			driver.close();
+	  @AfterMethod
+	  public void afterMethod() {
+		  driver.close();
+		 
+	  }
 
 	}
-
-}
