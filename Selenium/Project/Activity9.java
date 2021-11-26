4fbdab8befe6dccead66001201aa94f3b97c4bfb
govind_date07 <61514332+govinddate@users.noreply.github.com>
2021-11-27 01:54:44 +0530

    Update Activity9.java

--- Selenium/Project/Activity9.java
+++ Selenium/Project/Activity9.java
@@ -1,51 +1,55 @@
-package Project_Selenium;
+package Demo.Project_HRM;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 
-public class Activity9 {
+public class Activity_9 {
+	WebDriver driver;
+	  @Test
+	  public void f() throws InterruptedException {
+		  driver.findElement(By.id("txtUsername")).sendKeys("orange");
+		  driver.findElement(By.id("txtPassword")).sendKeys("orangepassword123");
+		  driver.findElement(By.id("btnLogin")).click();
+		  Thread.sleep(3000);
+	driver.findElement(By.id("menu_pim_viewMyDetails")).click();
+	Thread.sleep(2000);	
+	Actions actions = new Actions(driver);
+	WebElement mainMenu = driver.findElement(By.xpath("//*[@id=\"sidenav\"]/li[3]/a"));
+	actions.moveToElement(mainMenu);
+	   Thread.sleep(3000);
+	   mainMenu.click();
+	   Thread.sleep(2000);
+	   List<WebElement> allRows = driver.findElements(By.id("emgcontact_list")); 
+	   for (WebElement row : allRows) { 
+	       List<WebElement> cells = row.findElements(By.tagName("td")); 
+	       for (WebElement cell : cells) {         
+	       System.out.println(cell.getText());
+	   }
+	   }
+	   System.out.println("Activity 9 executed successfully");
+	  }//.....
+	  @BeforeMethod
+	  public void beforeMethod() {
+		  driver=new FirefoxDriver();
+		  driver.navigate().to("http://alchemy.hguy.co:8080/orangehrm/symfony/web/index.php/auth/login");
+	  }
 
-	@Test
-	public static void main() 
-	{
-		
-		WebDriver driver = new FirefoxDriver();
-		driver.get("https://alchemy.hguy.co/lms");
-		
-		driver.findElement(By.linkText("All Courses")).click();
-		
-		driver.findElement(By.xpath("//*[@id=\"post-69\"]/div[2]/p[2]/a")).click();
-		
-		driver.findElement(By.linkText("Login to Enroll")).click();
-		
-		
-		driver.findElement(By.id("user_login")).sendKeys("root");
-		driver.findElement(By.id("user_pass")).sendKeys("pa$$w0rd");
-		
-		driver.findElement(By.xpath("//*[@id=\"wp-submit\"]")).click();
-		
-		driver.findElement(By.xpath("//*[@id=\"ld-expand-83\"]/div[1]/a/div[2]")).click();
-		
-        String Title = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/h1")).getText();
-		
-        boolean status = Title.contentEquals("Developing Strategy");
-		
-		System.out.println("Title Matches: " + status);
-		
-		WebElement Button = driver.findElement(By.xpath("//*[@id=\"learndash_post_83\"]/div/div[4]/div[2]/a/span[1]"));
-		String ButtonContent = Button.getText();
-		boolean exist = ButtonContent.contentEquals("Mark Complete");
-		System.out.println("Mark Complete button displayed : " +exist);
-		
-		if(exist==true) {
-			Button.click();
-		}
-		
-		driver.close();
+	  @AfterMethod
+	  public void afterMethod() {
+		  driver.close();
+		 
+	  }
 
 	}
-}
