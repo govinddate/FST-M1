2e9194c5e28a58dfc63ab2fa2be56dde29d48134
govind_date07 <61514332+govinddate@users.noreply.github.com>
2021-11-27 01:48:03 +0530

    Update Activity2.java

--- Selenium/Project/Activity2.java
+++ Selenium/Project/Activity2.java
@@ -1,26 +1,36 @@
+package Demo.Project_HRM;
 
-package Project_Selenium;
-
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 
-public class Activity2 {
-@Test
-	public static void main() {
+public class Activity_2 {
+	WebDriver driver;
+  @Test
+  public void f() throws InterruptedException {
+	  WebElement wl=driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[1]/img"));
+	  System.out.println("Url of image is "+wl.getAttribute("src"));
+	  Thread.sleep(2000);
+	  System.out.println("Activity 2 executed successfully");
+	  
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  driver=new FirefoxDriver();
+	  driver.navigate().to("http://alchemy.hguy.co/orangehrm");
+	
+	  
+  }
 
-		WebDriver driver = new FirefoxDriver();
-		driver.get("https://alchemy.hguy.co/lms");
-		
-		String Title = driver.findElement(By.xpath("//*[@id=\"uagb-infobox-74cd79b6-b855-4e72-81bc-e70591de1896\"]/div/div/div/div[1]/h1")).getText();
-		
-        boolean status = Title.contentEquals("Learn from Industry Experts");
-		
-		System.out.println("Title Matches: " + status);
-		
-		
-
-	}
+  @AfterMethod
+  public void afterMethod() {
+	  driver.close();
+	  
+  }
 
 }
