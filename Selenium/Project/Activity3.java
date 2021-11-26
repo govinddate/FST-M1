31eae287003970b9d40da364b410c82fd016f700
govind_date07 <61514332+govinddate@users.noreply.github.com>
2021-11-27 01:49:49 +0530

    Update Activity3.java

--- Selenium/Project/Activity3.java
+++ Selenium/Project/Activity3.java
@@ -12,7 +12,6 @@ public class Activity3 {
 		
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://alchemy.hguy.co/lms");
-		
 		String Title = driver.findElement(By.xpath("//*[@id=\"uagb-infobox-f08ebab0-fbf1-40ec-9b2a-c9feeb3d4810\"]/div/div/div/div[2]/h3")).getText();
 		
         boolean status = Title.contentEquals("Actionable Training");
