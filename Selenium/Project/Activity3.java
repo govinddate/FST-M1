c54e0e296907f9da56ba6a6f55cd1025ec938de9
govind_date07 <61514332+govinddate@users.noreply.github.com>
2021-11-27 01:57:08 +0530

    Update Activity3.java

--- Selenium/Project/Activity3.java
+++ Selenium/Project/Activity3.java
@@ -1,26 +1,44 @@
-package Project_Selenium;
+package Demo.Project_HRM;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 
-public class Activity3 {
+public class Activity_3 {
+	WebDriver driver;
+  @Test
+  public void f() throws InterruptedException {
+	  driver.findElement(By.id("txtUsername")).sendKeys("orange");
+	  driver.findElement(By.id("txtPassword")).sendKeys("orangepassword123");
+	  driver.findElement(By.id("btnLogin")).click();
+	  Thread.sleep(3000);
+	  String hp="menu_dashboard_index";
+	  if(driver.findElement(By.id(hp)).isDisplayed())
+	  {
+		  System.out.println("Homepage is display");
+		 
+	  }
+	  else
+	  {
+		 
+	  }
+	  System.out.println("Activity 3 executed successfully");
+	 
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  driver=new FirefoxDriver();
+	  driver.navigate().to("http://alchemy.hguy.co:8080/orangehrm/symfony/web/index.php/auth/login");
+  }
 
-	@Test
-	public static void main() {
-		
-		WebDriver driver = new FirefoxDriver();
-		driver.get("https://alchemy.hguy.co/lms");
-		String Title = driver.findElement(By.xpath("//*[@id=\"uagb-infobox-f08ebab0-fbf1-40ec-9b2a-c9feeb3d4810\"]/div/div/div/div[2]/h3")).getText();
-		
-        boolean status = Title.contentEquals("Actionable Training");
-		
-		System.out.println("Title Matches: " + status);
-		
-		driver.close();
-
-
-	}
+  @AfterMethod
+  public void afterMethod() {
+	  driver.close();
+	 
+  }
 
 }
