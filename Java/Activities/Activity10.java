f7abe3353cf871662f273039a7ed0a8d062e6065
govind_date07 <61514332+govinddate@users.noreply.github.com>
2021-11-27 10:16:30 +0530

    Update Activity10.java

--- Java/Activities/Activity10.java
+++ Java/Activities/Activity10.java
@@ -6,28 +6,28 @@ public class Activity10 {
     public static void main(String[] args) {
         HashSet<String> hs = new HashSet<String>();
         
-        hs.add("Vidhya");
-        hs.add("Bala");
-        hs.add("keerthi");
-        hs.add("Jothi");
-        hs.add("Anya");
-        hs.add("Ramya");
+        hs.add("asd");
+        hs.add("dfgdfgdfg");
+        hs.add("gfhgh");
+        hs.add("dffg");
+        hs.add("wer");
+        hs.add("ytyy");
         
         
         System.out.println("Original HashSet: " + hs);        
        
         System.out.println("Size of HashSet: " + hs.size());
     
-        System.out.println("Removing A from HashSet: " + hs.remove("Jothi"));
+        System.out.println("Removing A from HashSet: " + hs.remove("asd"));
        
-        if(hs.remove("Z")) {
+        if(hs.remove("e")) {
         	System.out.println("Z removed from the Set");
         } else {
         	System.out.println("Z is not present in the Set");
         }
         
         
-        System.out.println("Checking if Anya is present: " + hs.contains("Anya"));
+        System.out.println("Checking if ytyy is present: " + hs.contains("ytyy"));
     
         System.out.println("Updated HashSet: " + hs);
     }
