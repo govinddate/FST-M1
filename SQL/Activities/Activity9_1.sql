f4ae939f10252c95719a22fb93254b6a1bbc38b5
govind_date07 <61514332+govinddate@users.noreply.github.com>
2021-11-27 10:29:23 +0530

    Update Activity9_1.sql

--- SQL/Activities/Activity9_1.sql
+++ SQL/Activities/Activity9_1.sql
@@ -36,4 +36,3 @@ insert into customers values
 (3005, 'Graham Zusi', 'California', 200, 5002), (3008, 'Julian Green', 'London', 300, 5002), 
 (3004, 'Fabian Johnson', 'Paris', 300, 5006), (3009, 'Geoff Cameron', 'Berlin', 100, 5003), 
 (3003, 'Jozy Altidor', 'Moscow', 200, 5007), (3001, 'Brad Guzan', 'London', 300, 5005);
-
